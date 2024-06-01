1f6cf34bbcbb04de566833b472a701d799cd7d50
Emmett <emmettirl@gmail.com>
2024-06-01 22:44:17 +0100

    Completed tutorial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project Boost
+
+This is a simple game made in Godot, following the tutorial from the "Complete Godot 3D: Code Your Own 3D Games in Godot" course on GameDev.tv
+https://www.gamedev.tv/dashboard/courses/101
---
+++ scripts/Player.gd
@@ -0,0 +1,76 @@
+extends RigidBody3D
+
+## How much vertical force to apply when moving
+@export_range(750.0, 3000.0) var thrust: float = 1000.0
+@export var torque_thrust: float = 100.0
+
+var is_transitioning: bool = false
+
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var booster_particles_right: GPUParticles3D = $BoosterParticlesRight
+@onready var booster_particles_left: GPUParticles3D = $BoosterParticlesLeft
+@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
+@onready var success_particles: GPUParticles3D = $SuccessParticles
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    if Input.is_action_pressed("boost"):
+        apply_central_force(basis.y * delta * thrust)
+        booster_particles.emitting = true
+        if rocket_audio.playing == false:
+            rocket_audio.play()
+    else:
+        booster_particles.emitting=false
+        if rocket_audio.playing == true:
+            rocket_audio.stop()
+       
+    if Input.is_action_pressed("rotate_left") && Input.is_action_pressed("rotate_right"):
+        booster_particles_left.emitting=true
+        booster_particles_right.emitting=true
+    elif Input.is_action_pressed("rotate_left"):
+        apply_torque(Vector3(0.0,0.0,torque_thrust)* delta)
+        booster_particles_left.emitting=true
+        booster_particles_right.emitting=false
+    elif Input.is_action_pressed("rotate_right"):
+        apply_torque(Vector3(0.0,0.0,-torque_thrust)* delta)
+        booster_particles_right.emitting=true
+        booster_particles_left.emitting=false
+    else:
+        booster_particles_left.emitting=false
+        booster_particles_right.emitting=false
+
+    if Input.is_action_pressed("ui_cancel"):
+        get_tree().quit()
+
+func _on_body_entered(body: Node) -> void:
+    if !is_transitioning:
+        if "Goal" in body.get_groups():
+            complete_level(body.file_path)
+        elif "Hazard" in body.get_groups():
+            crash_sequence()
+
+func crash_sequence() -> void:
+    print("KABOOM!")
+    explosion_audio.play()
+    explosion_particles.emitting = true
+    
+    set_process(false)
+    is_transitioning = true
+    var tween = create_tween()
+    tween.tween_interval(2.5)
+    tween.tween_callback(get_tree().reload_current_scene)
+
+func complete_level(next_level_file:String) -> void:
+    print("You Win!")
+    success_audio.play()
+    success_particles.emitting = true
+    
+    set_process(false)
+    is_transitioning = true
+    var tween = create_tween()
+    tween.tween_interval(1.0)
+    tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
+    
